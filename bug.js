17b40cd242c080821819bb1399a71847d503ad49
BugHunter-X <askzenai@gmail.com>
2025-02-05 21:18:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,19 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs after every render
+    console.log('Count:', count); 
+    //In this case it will trigger infinite loop
+    setCount(count + 1); 
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
